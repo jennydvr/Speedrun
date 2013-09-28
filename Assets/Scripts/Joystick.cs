96cf5050785c05d9502772e46452e229d250ff06
Jenny Valdez <jedaniv@gmail.com>
2013-09-28 16:38:19 -0430

    [Transferencia parte 1]

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -5,44 +5,31 @@ public class Joystick : MonoBehaviour {
 
     #region Variables
 
-    /// <summary>
-    /// Puedo mover a la reina?
-    /// </summary>
     public static bool CanMoveQueen = false;
+    public static Ray ScreenRay;
 
     #endregion
 
     #region Unity
 
-	/// <summary>
-    /// Actualizacion
-    /// </summary>
 	private void Update () {
 
         // Boton izquierdo pulsado
         if (Input.GetMouseButtonDown(0)) {
-
             // Lanzo un rayo para ver con que choco
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            ScreenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             // Si toco un collider y es un planeta, seleccionar
-            if (Planet.SelectedPlanets.Count < 2 && Physics.Raycast(ray, out hit)) {
-                Planet planet = hit.collider.gameObject.GetComponent<Planet>();
-                planet.PlanetSelection(true);
+            if (Planet.CanSelectPlanet() && Physics.Raycast(ScreenRay, out hit)) {
+                // No puedo mover a la reina
+                CanMoveQueen = false;
 
-                if (Planet.SelectedPlanets.Count == 2)
-                    Planet.Transfer();
-
+                // Reviso si estoy seleccionando un planeta
+                Planet.IsPlanetSelected(hit.collider.GetComponent<Planet>());
             } else {
-                // Deseleccionar planeta seleccionado
-                if (Planet.SelectedPlanets.Count == 1)
-                {
-                    Planet.SelectedPlanets[0].PlanetSelection(false);
-                }
-
-                // Puedo mover a la reina
-                CanMoveQueen = true;
+                // Puedo mover a la reina de acuerdo a si estoy o no deseleccionando un planeta
+                CanMoveQueen = Planet.DeselectingLastPlanet();
             }
         }
 	}
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,54 +5,32 @@ public class Planet : MonoBehaviour {
 
     #region Static Variables
 
-    /// <summary>
-    /// Lista de planetas en seleccion
-    /// </summary>
-    public static List<Planet> SelectedPlanets = new List<Planet>();
+    private static List<Planet> SelectedPlanets = new List<Planet>();
 
-    /// <summary>
-    /// Tiempo de transferencia
-    /// </summary>
     public static float TransferTime = 1.5f;
+
+    public LayerMask QueenLayer = -1, ObstaclesLayer = -1;
 
     #endregion
 
-    #region Private Variables
+    #region Instance Variables
 
-    /// <summary>
-    /// Mi transform
-    /// </summary>
     private Transform Me;
 
-    /// <summary>
-    /// Indica si el planeta se encuentra seleccionado
-    /// </summary>
     private bool Selected = false;
-    
-    /// <summary>
-    /// Indica si estamos transfiriendo
-    /// </summary>
+
     private bool Transfering = false;
 
-    /// <summary>
-    /// Tiempo inicial de transferencia
-    /// </summary>
     private float StartTransferTime;
 
     #endregion
 
     #region Unity
 
-    /// <summary>
-    /// Inicializacion
-    /// </summary>
 	private void Start() {
         Me = transform;
 	}
 
-    /// <summary>
-    /// Actualizacion
-    /// </summary>
     private void Update() {
         if (Transfering && Time.time - StartTransferTime >= TransferTime) {
             EndTransfer();
@@ -63,10 +41,6 @@ public class Planet : MonoBehaviour {
 
     #region Public Methods
 
-    /// <summary>
-    /// Selecciona / deselecciona un planeta
-    /// </summary>
-    /// <param name="select">Seleccionar si esta en <c>true</c></param>
     public void PlanetSelection(bool select) {
         // No seleccionamos al transferir
         if (Transfering || Selected == select)
@@ -81,18 +55,12 @@ public class Planet : MonoBehaviour {
             SelectedPlanets.Remove(this);
     }
 
-    /// <summary>
-    /// Iniciar transferencia
-    /// </summary>
     public void StartTransfer() {
         Transfering = true;
         renderer.material.color = Color.blue;
         StartTransferTime = Time.time;
     }
 
-    /// <summary>
-    /// Termina la transferencia
-    /// </summary>
     public void EndTransfer() {
         Transfering = false;
         PlanetSelection(false);
@@ -102,12 +70,53 @@ public class Planet : MonoBehaviour {
 
     #region Static Functions
 
-    /// <summary>
-    /// Transfiere cosas de un planeta a otro
-    /// </summary>
+    public static bool CanSelectPlanet() {
+        return Planet.SelectedPlanets.Count < 2;
+    }
+
+    public static bool DeselectingLastPlanet() {
+        if (SelectedPlanets.Count == 1) {
+            SelectedPlanets[0].PlanetSelection(false);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void IsPlanetSelected(Planet planet) {
+        // Si no hay componente planeta o ya se han seleccionado dos planetas
+        if (planet == null || SelectedPlanets.Count == 2)
+            return;
+
+        // Selecciono el planeta
+        planet.PlanetSelection(true);
+
+        // Si ya hay dos planetas, hacer transferencia
+        if (Planet.SelectedPlanets.Count == 2)
+            Planet.Transfer();
+    }
+
     public static void Transfer() {
-        SelectedPlanets[0].StartTransfer();
-        SelectedPlanets[1].StartTransfer();
+        Planet one = SelectedPlanets[0];
+        Planet two = SelectedPlanets[1];
+
+        // Chequear que la reina se encuentre de por medio
+        Vector3 onePos = one.transform.position;
+        Vector3 twoPos = two.transform.position;
+        Vector3 diff = twoPos - onePos;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(onePos, diff, out hit, diff.magnitude, one.QueenLayer)) {
+            // Iniciar transferencia
+            one.StartTransfer();
+            two.StartTransfer();
+
+        } else {
+            // Deselecciono los planetas
+            one.PlanetSelection(false);
+            two.PlanetSelection(false);
+        }
     }
 
     #endregion
